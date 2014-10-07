a77a8c73345590cacee9881b600d9427e7b04068
raghav154 <raghavs.154@gmail.com>
2014-10-07 08:11:36 -0500

    select tweaking needed

--- tftp-server/server_bckup_select.c
+++ tftp-server/server_bckup_select.c
@@ -41,9 +41,8 @@ char error_code []={	0 /*Not defined, see error message (if any)"*/,
 #define RETRIES 50 /* 5 sec to terminate and 100 ms ACK timeout (5000/100 = 50) */ 
 #define ACK_TIMEOUT 100000 /* 100 ms = 100000 usec */
 #define MAXDATASIZE 512
-#define MAXCLIENTS 100 /* Assume a maximum of 100 clients */
 
-char cwd[100]; /* The PATH to the current working directory */
+char cwd[100];
 
 // Send file 
 void tftp_sendfile (char *file_name, int org_sock, struct sockaddr_in client, char *transfer_mode, int tid)
@@ -231,17 +230,12 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, sockmax, i, rv, len, sock, client_len, ssize = 0, n, j, numbytes, opcode; 
-    unsigned short int count[MAXCLIENTS], rcount = 0, resend =0; 
+    int sockfd, new_fd, sockmax, i, rv;  
     struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_in client_addr, cl_addr[MAXCLIENTS], ack; 
+    struct sockaddr_in client_addr; // connector's address information
     socklen_t client_size;
-    struct timeval curr_time, time_now;
-    char buf[MAXDATASIZE], filebuf[MAXDATASIZE], sendbuf[MAXDATASIZE + 4], recvbuf[MAXDATASIZE];
-    char filename[100], mode[10], file_path[150], *index, *ptr;
-
-    // pointer to the file we will be sending
-    FILE *fp[MAXCLIENTS]; 
+    int numbytes,opcode;
+    char *index,buf[MAXDATASIZE],mode[12],filename[256];
  
     //Checking specification of command line options
     if (argc != 3) {
@@ -284,33 +278,31 @@ int main(int argc, char *argv[])
     freeaddrinfo(servinfo); // all done with this structure
 
     // FD_SET variables for select() 
-    fd_set master, master_write, read_fds, write_fds ;
+    fd_set master,read_fds;
 
     // clear the master and temp sets
     FD_ZERO(&master);    
-    FD_ZERO(&read_fds); 
-    FD_ZERO(&master_write);    
-    FD_ZERO(&write_fds);	
+    FD_ZERO(&read_fds);
 
     // add the socket descriptor to the master set
     FD_SET(sockfd, &master);
-    //FD_SET(sockfd, &master_write);
 
     // keep track of the largest socket descriptor to use with select()
     sockmax = sockfd; // so far, it's this one
 
     printf("TFTP server started ....\n");
-
+/*    struct timeval tv;
+    tv.tv_sec = 5;
+*/
     while(1) 
     {  // main accept() loop
 	read_fds = master;
-	write_fds = master_write;
-	if(select(sockmax+1,&read_fds,&write_fds,NULL,NULL) == -1) {
+	if(select(sockmax+1,&read_fds,NULL,NULL,NULL) == -1) {
 	        printf("Error with select \n");
         	perror("select");
 	        exit(1);
     	}
-	
+
 	//Looping through all incoming socket connections 
 	for(i=0; i<=sockmax; i++) {
 		if(FD_ISSET(i, &read_fds)) {
@@ -342,45 +334,20 @@ int main(int argc, char *argv[])
 						// Moving the index further 
 						index += strlen (mode) + 1; 
 						// Print decoded values from the TFTP frame       					
-						printf ("server: opcode: %x filename: %s packet size: %d mode: %s\n", opcode, filename, numbytes, mode);   
+						printf ("opcode: %x filename: %s packet size: %d mode: %s\n", opcode, filename, numbytes, mode);   
 		    				printf ("server: READ REQUEST\n");
-						// Obtaining Current directory
-						if (getcwd(cwd, sizeof(cwd)) != NULL)
-							printf("server: Current working directory: %s\n", cwd);
-						else
-							perror("server: getcwd() error");
-	
 						// Opening a new socket sending the file
-						if ((sock = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)   
+						/*if ((new_fd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)   
 					    	{
 							printf ("server: Error in creating socket for sending file \n");
-						      	continue;
-						}
-
-						//Appending the filename to the path
-						sprintf(file_path,"%s/",cwd);
-						strncat (file_path, filename, sizeof (file_path) - 1);  
-						fp[sock-sockfd-1] = fopen (file_path, "r");
-						cl_addr[sock-sockfd-1] = client_addr;
-						if (fp[sock-sockfd-1] == NULL) {              
-							printf ("server: file not found (%s)\n", file_path);
-							len = sprintf ((char *) sendbuf, "%c%c%c%cFile not found (%s)%c", 0x00, ERR, 0x00, error_code[1], file_path, 0x00);
-							//Sending Error PCKT to client
-							if (sendto (sock, sendbuf, len, 0, (struct sockaddr *) &cl_addr[sock-sockfd-1], sizeof (cl_addr[sock-sockfd-1])) != len)
-								printf("server: file not found mode error packet not sent correctly\n");
-		
-							continue;
-						}
-					  	
-						else {
-					      	        printf ("server: Sending file... (source: %s)\n", file_path);
-							count[sock-sockfd-1] = 0;
-							FD_SET(sock, &master); 
-							FD_SET(sock, &master_write);
-							if(sock > sockmax) {
-								sockmax = sock;
-						 	}	
-					    	}
+						      	return 0;
+						}*/
+							
+						/*FD_SET(new_fd, &master); //Adding to master set
+						if(new_fd > sockmax) {
+							sockmax = new_fd;
+					 	}*/	
+						tftp_sendfile (filename, sockfd, client_addr, mode, ntohs (client_addr.sin_port));
 					}			
 					else
 					{
@@ -389,84 +356,11 @@ int main(int argc, char *argv[])
 					}
 				}
 			}
-			// From a client socket
-			else {
-				client_len = sizeof (ack);
-				resend = 0;
-				// per packet ACK timeout		
-				gettimeofday(&time_now, NULL);
-				do {
-					if((n = recvfrom (i, recvbuf, sizeof (recvbuf), MSG_DONTWAIT, (struct sockaddr *) &ack, (socklen_t *) & client_len)) > 0)
-						break;
-					// Continue to check current time to ensure ACKs don't timeout
-					gettimeofday(&curr_time, NULL);
-
-				}while (((curr_time.tv_sec - time_now.tv_sec)*1000000 + curr_time.tv_usec - time_now.tv_usec) < ACK_TIMEOUT);
-
-				if (n < 0 )
-					resend = 1;
-
-				else {
-					// Decode received message correctly
-					index = (char *) recvbuf;  
-					index++; // moving past first null byte
-					opcode = *index++;
-					rcount = *index++ << 8;
-					rcount &= 0xff00;
-					rcount |= (*index++ & 0x00ff);
-					// Checking the opcode and the block number of the ACK for the sent packet
-					if (opcode != ACK || rcount != count[i-sockfd-1]) {
-						printf ("server: Remote host failed to ACK proper data packet # %d (got OP: %d Block: %d)\n", count[i-sockfd-1], opcode, rcount);
-						// Send Error
-						if (opcode > ERR) {						
-							len = sprintf ((char *) recvbuf, "%c%c%c%cIllegal operation%c", 0x00, ERR, 0x00, error_code[4], 0x00);
-							if (sendto (i, recvbuf, len, 0, (struct sockaddr *) &ack, sizeof (ack)) != len)      
-			   	                        	printf ("server: Error in ACK packet not sent correctly\n");
-						}
-					}
-		  			else {
-						printf ("server: ACK successfully received for block (#%d)\n", rcount);
-						FD_SET(i,&master_write);
-		    			}
-				}		 					 	 		
-			}
+			/*else {
+				 
+				FD_CLR(new_fd,&master); 		
+			}*/
 		}
-		if (FD_ISSET(i,&write_fds)) {
-			if(resend == 1) {
-				// resending data packet
-				if (sendto (i, sendbuf, len, 0, (struct sockaddr *) &cl_addr[sock-sockfd-1], sizeof (cl_addr[sock-sockfd-1])) != len) {
-					printf ("server: Error resending data packet\n");
-					continue;
-	    			}
-	    			printf ("server: ACK lost. Resending Packet: %d\n", count[i-sockfd-1]);
-				continue;					
-			}
-			ssize = fread (filebuf, 1, MAXDATASIZE, fp[i-sockfd-1]);
-			count[i-sockfd-1]++; 
-			count[i-sockfd-1] = htons(count[i-sockfd-1]); 
-			ptr = (char *) &count[i-sockfd-1];
-			sprintf ((char *) sendbuf, "%c%c", 0x00, DATA);  
-			memcpy ((char *) sendbuf + 2, ptr, 2); 
-			memcpy ((char *) sendbuf + 4, filebuf, ssize);
-			len = 4 + ssize;
-			count[i-sockfd-1] = ntohs(count[i-sockfd-1]);
-			printf ("server: Sending packet # %d (length: %d file chunk: %d)\n", count[i-sockfd-1], len, ssize);
-
-			// Sending the data packet
-			if (sendto (i, sendbuf, len, 0, (struct sockaddr *) &cl_addr[sock-sockfd-1], sizeof (cl_addr[sock-sockfd-1])) != len) {
-				printf("server: data packet not sent correctly\n");
-				continue;
-			}	
-			FD_CLR(i,&master_write);
-			if (ssize != MAXDATASIZE) {
-			    	fclose (fp[i-sockfd-1]);
-				count[i-sockfd-1] = 0;
-				close (i);
-				FD_CLR(i,&master);
-				printf ("server: File sent successfully\n");				
-			}
-			memset (filebuf, 0, sizeof (filebuf));
-		}    
 	}
    }
    return 0;
