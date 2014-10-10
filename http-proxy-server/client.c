55dc4314638a271fb2478e951b7e4d4d048dafb4
raghav154 <raghavs.154@gmail.com>
2014-10-10 14:06:43 -0500

    http proxy

---
+++ http-proxy-server/client.c
@@ -0,0 +1,19 @@
+/* 
+ECEN602: Computer Networks
+HW3 programming assignment: HTTP1.0 client GET request
+*/
+
+int main(int argc, char const *argv[])
+{
+	/* write HTTP1.0 client code */
+	/* example of GET request for www.google.com*/
+
+	    char query[] =
+        "GET / HTTP/1.0\r\n"
+        "Host: www.google.com\r\n"
+        "\r\n";
+
+    
+
+	return 0;
+}
---
+++ http-proxy-server/proxy.c
@@ -0,0 +1,11 @@
+/* 
+ECEN602: Computer Networks
+HW3 programming assignment: HTTP1.0 proxy server
+*/
+
+int main(int argc, char const *argv[])
+{
+	/* write HTTP1.0 proxy server code */
+
+	return 0;
+}
