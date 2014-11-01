6825eb1b9b755c5fda1bc4b0e7ba7df38f53785b
raghav154 <raghavs.154@gmail.com>
2014-11-01 17:53:10 -0500

    ns2-sim

---
+++ ns2_net/ns2.tcl
@@ -0,0 +1,104 @@
+# Write ns2.tcl code for specified network
+# usage:
+# ns ns2.tcl <TCP_VERSION> <CASE_NO>
+# example: ns ns2.tcl Vegas 2
+
+set ns [new Simulator]
+
+set f0 [open src1_[lindex $argv 0]_[lindex $argv 1].tr w]
+set f1 [open src2_[lindex $argv 0]_[lindex $argv 1].tr w]
+
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+proc finish {} {
+	global f0 f1 nf
+	close $f0
+	close $f1
+	exec nam out.nam &
+	exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+$ns duplex-link $n0 $n2 10Mb 5ms DropTail
+$ns duplex-link $n3 $n2 1Mb 5ms DropTail
+$ns duplex-link $n4 $n3 10Mb 5ms DropTail
+
+if {[lindex $argv 1] == 1} {
+$ns duplex-link $n1 $n2 10Mb 12.5ms DropTail
+$ns duplex-link $n5 $n3 10Mb 12.5ms DropTail
+} elseif {[lindex $argv 1] == 2} {
+$ns duplex-link $n1 $n2 10Mb 20.0ms DropTail
+$ns duplex-link $n5 $n3 10Mb 20.0ms DropTail
+} else {
+$ns duplex-link $n1 $n2 10Mb 27.5ms DropTail
+$ns duplex-link $n5 $n3 10Mb 27.5ms DropTail
+}
+
+#Create a TCP agent and attach it to node n0
+set tcp0 [new Agent/TCP/[lindex $argv 0]]
+$ns attach-agent $n0 $tcp0
+
+# Create a FTP traffic source and attach it to tcp0
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
+
+#Create a TCP agent and attach it to node n1
+set tcp1 [new Agent/TCP/[lindex $argv 0]]
+$ns attach-agent $n1 $tcp1
+
+# Create a FTP traffic source and attach it to tcp1
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+
+set sink0 [new Agent/TCPSink]
+set sink1 [new Agent/TCPSink]
+$ns attach-agent $n4 $sink0
+$ns attach-agent $n5 $sink1
+
+$ns connect $tcp0 $sink0
+$ns connect $tcp1 $sink1
+
+proc record {} {
+	global sink0 sink1 f0 f1 
+	#Get an instance of the simulator	
+	set ns [Simulator instance]
+	#Set the time after which the procedure should be called 		again	
+	set time 0.01
+	#How many bytes have been received by the traffic 		sinks?	
+	set bw0 [$sink0 set bytes_]
+	set bw1 [$sink1 set bytes_]
+	#Get the current time
+	set now [$ns now]
+	#Calculate the bandwidth (in MBit/s) and write it to the 		files
+        puts $f0 "$now [expr $bw0/$time*8/1000000]"
+        puts $f1 "$now [expr $bw1/$time*8/1000000]"
+	#Reset the bytes_ values on the traffic sinks
+        $sink0 set bytes_ 0
+        $sink1 set bytes_ 0	
+	#Re-schedule the procedure
+        $ns at [expr $now+$time] "record"
+	
+}
+
+$ns at 0.0 "record"
+$ns at 0.0 "$ftp0 start"
+$ns at 0.0 "$ftp1 start"
+$ns at 40.0 "$ftp0 stop"
+$ns at 40.0 "$ftp1 stop"
+
+$tcp0 set class_ 1
+$tcp1 set class_ 2
+$ns color 1 Blue
+$ns color 2 Red
+
+$ns at 40.0 "finish"
+
+$ns run
+
